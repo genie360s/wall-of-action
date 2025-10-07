7ee653728cf264bcc3495b19f7c93e3c394cbbe1
genie360s <alexgmkwizu@gmail.com>
2025-10-08 02:06:15 +0300

    fix: date to show current date

--- web-app/src/App.vue
+++ web-app/src/App.vue
@@ -3,10 +3,12 @@ import { RouterLink, RouterView } from 'vue-router'
 </script>
 
 <template>
-  <div class="container mx-auto flex space-grotesk-font gap-[31.65vw] mb-4 mt-4"> 
+  <div class="container mx-auto flex space-grotesk-font justify-between mb-4 mt-4"> 
     <div><RouterLink to="/"><img alt="Bowseat logo" class="logo" src="@/assets/bowseat-logo.svg" width="100" height="100" /></RouterLink></div> 
     <div class="text-lg font-bold"> Wall Of Action </div>
-    <div> 19-August-2025 </div>
+    <div class="text-sm">
+      {{ new Date().toLocaleDateString('en-US', { month: 'short', day: '2-digit', year: 'numeric' }).replace(/\//g, '-').replace(',', '') }}
+    </div>
   </div>
   <div class="flex space-grotesk-font container mx-auto w-full text-sm border-b border-t border-blue-800 p-2"> 
     <div class="w-1/3"><div class="p-2 text-sm font-bold hover:text-blue-800"><RouterLink to="/thewall">The Wall</RouterLink></div></div>
