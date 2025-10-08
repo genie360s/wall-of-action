3ee02ffba5efead98cc49f81145e8e98506d003e
genie360s <alexgmkwizu@gmail.com>
2025-10-08 20:26:02 +0300

    add : responsiveness for navbar

--- web-app/src/App.vue
+++ web-app/src/App.vue
@@ -3,21 +3,23 @@ import { RouterLink, RouterView } from 'vue-router'
 </script>
 
 <template>
-  <div class="container mx-auto flex space-grotesk-font justify-between mb-4 mt-4"> 
-    <div><RouterLink to="/"><img alt="Bowseat logo" class="logo" src="@/assets/bowseat-logo.svg" width="100" height="100" /></RouterLink></div> 
-    <div class="text-lg font-bold"> Wall Of Action </div>
-    <div class="text-sm">
+  
+  <div class="container mx-auto w-full flex space-grotesk-font justify-between mb-4 mt-4 "> 
+    <div class="xs:ml-0 ml-2"><RouterLink to="/"><img alt="Bowseat logo" class="logo" src="@/assets/bowseat-logo.svg" width="100" height="100" /></RouterLink></div> 
+    <div class="xs:text-lg text-sm font-bold"> Wall Of Action </div>
+    <div class="text-sm mr-2 xs:mr-0">
       {{ new Date().toLocaleDateString('en-US', { month: 'short', day: '2-digit', year: 'numeric' }).replace(/\//g, '-').replace(',', '') }}
     </div>
   </div>
-  <div class="flex space-grotesk-font container mx-auto w-full text-sm border-b border-t border-blue-800 p-2"> 
-    <div class="w-1/3"><div class="p-2 text-sm font-bold hover:text-blue-800"><RouterLink to="/thewall">The Wall</RouterLink></div></div>
-    <div class="flex gap-16 w-2/3">
-      <div class="border border-blue-800 p-2  text-sm font-medium"> <p class="mx-auto">Countries <span class="font-bold">89</span></p> </div>
-      <div class="border border-blue-800 p-2  text-sm font-medium"> <p class="mx-auto">Actors <span class="font-bold">89</span></p> </div>
-      <div class="border border-blue-800 p-2  text-sm font-medium"> <p class="mx-auto">Actions <span class="font-bold">89</span></p> </div>
+  <div class="flex  space-grotesk-font container mx-auto w-full text-sm border-b border-t border-blue-800 p-2"> 
+    <div class="xs:w-1/3 w-1/5"><div class="p-2 text-sm font-bold hover:text-blue-800"><RouterLink to="/thewall">The Wall</RouterLink></div></div>
+    <div class="xs:w-2/3 w-4/5 flex xs:gap-16 gap-4 ">
+      <div class="border border-blue-800 p-2 text-xs xs:text-sm font-medium"> <p class="text-center">Countries <span class="font-bold">89</span></p> </div>
+      <div class="border border-blue-800 p-2 text-xs xs:text-sm font-medium"> <p class="text-center">Actors <span class="font-bold">89</span></p> </div>
+      <div class="border border-blue-800 p-2 text-xs xs:text-sm font-medium"> <p class="text-center">Actions <span class="font-bold">89</span></p> </div>
     </div>
   </div>
 
   <RouterView />
+  
 </template>
